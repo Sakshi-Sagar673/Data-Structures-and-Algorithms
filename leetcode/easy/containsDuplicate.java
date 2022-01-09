7a621d729af56863f29dabf22d75aa185c52dcee
Sakshi Sagar <85617513+Sakshi-Sagar673@users.noreply.github.com>
2022-01-09 16:54:14 +0530

    Create containsDuplicate.java

---
+++ leetcode/easy/containsDuplicate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!set.contains(nums[i]))
+            {
+                set.add(nums[i]);
+            }
+            else
+            {
+                return true;
+            }
+        }
+       return false;
+    }
+}
