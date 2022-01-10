4be1fc98905e3ec26626bc68d8358525c3485cc1
Sakshi Sagar <85617513+Sakshi-Sagar673@users.noreply.github.com>
2022-01-10 15:16:25 +0530

    Create FindAllNumbersDisappearedinanArray.java

---
+++ leetcode/easy/FindAllNumbersDisappearedinanArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!set.contains(nums[i]))
+            {
+                set.add(nums[i]);
+            }
+        }
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(!set.contains(i))
+            {
+                ans.add(i);
+            }
+        }
+        return ans;
+        
+    }
+}
