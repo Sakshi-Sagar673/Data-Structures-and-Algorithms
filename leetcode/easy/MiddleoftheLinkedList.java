ae34763c5e1ec0c4faf1c0d26ec457ae0a11627b
Sakshi Sagar <85617513+Sakshi-Sagar673@users.noreply.github.com>
2022-01-14 13:03:05 +0530

    Update MiddleoftheLinkedList.java

--- leetcode/easy/MiddleoftheLinkedList.java
+++ leetcode/easy/MiddleoftheLinkedList.java
@@ -21,3 +21,24 @@ class Solution {
         
     }
 }
+
+//Alternate Solution
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int l=0,c=0;
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            l=l+1;
+            temp=temp.next;
+        }
+        temp=head;
+        while(c<l/2)
+        {
+            temp=temp.next;
+            c=c+1;
+        }
+        return temp;
+    }
+}
