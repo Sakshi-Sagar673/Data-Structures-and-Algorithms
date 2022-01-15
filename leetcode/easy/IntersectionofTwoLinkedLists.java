f35520516c1221a4f6367ec883b95b548094ab63
Sakshi Sagar <85617513+Sakshi-Sagar673@users.noreply.github.com>
2022-01-15 12:34:40 +0530

    Update IntersectionofTwoLinkedLists.java

--- leetcode/easy/IntersectionofTwoLinkedLists.java
+++ leetcode/easy/IntersectionofTwoLinkedLists.java
@@ -50,3 +50,20 @@ public class Solution {
         return null;
     }
 }
+//Optimized approach
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null)
+        {
+            return null;
+        }
+        ListNode a=headA;
+        ListNode b=headB;
+        while(a!=b)
+        {
+            a=a==null?headB:a.next;
+            b=b==null?headA:b.next;
+        }
+        return a;
+    }
+}
