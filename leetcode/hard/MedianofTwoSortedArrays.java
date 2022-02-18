41b52df529ac4cace155ee2fc6c280663055fa53
Sakshi Sagar <85617513+Sakshi-Sagar673@users.noreply.github.com>
2022-02-18 12:13:40 +0530

    Update MedianofTwoSortedArrays.java

--- leetcode/hard/MedianofTwoSortedArrays.java
+++ leetcode/hard/MedianofTwoSortedArrays.java
@@ -43,3 +43,51 @@ class Solution {
         return med;
     }
 }
+
+//optimised solution
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       if(nums2.length<nums1.length)
+           return findMedianSortedArrays(nums2,nums1);
+        int s1,s2,l1,l2,r1,r2,lo,hi,mid1,mid2;
+        s1=nums1.length;
+        s2=nums2.length;
+        lo=0;
+        hi=s1;
+        while(lo<=hi)
+        {
+            mid1=lo+(hi-lo)/2;
+            mid2=(s1+s2+1)/2-mid1;
+            
+            l1=mid1==0?Integer.MIN_VALUE:nums1[mid1-1];
+            l2=mid2==0?Integer.MIN_VALUE:nums2[mid2-1];
+            
+            r1=mid1==s1?Integer.MAX_VALUE:nums1[mid1];
+            r2=mid2==s2?Integer.MAX_VALUE:nums2[mid2];
+            
+            if(l1<=r2 && l2<=r1)
+            {
+                if((s1+s2)%2==0)
+                {
+                    return ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
+                }
+                else
+                {
+                    return (Math.max(l1,l2));
+                }
+            }
+            else if(l1>r2)
+            {
+                hi=mid1-1;
+            }
+            else
+            {
+                lo=mid1+1;
+            }
+            
+        }
+        return 0.0;
+    }
+}
+        
